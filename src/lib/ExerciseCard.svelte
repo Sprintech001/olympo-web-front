231d312287402fe90ee0228954ee92d85acba525
Thomaz Rodrigues <thomazrodrigues668@gmail.com>
2025-03-29 17:11:41 -0300

    feat(view): exclusão e inclusão de alunos aos exercícios

--- src/lib/ExerciseCard.svelte
+++ src/lib/ExerciseCard.svelte
@@ -2,7 +2,32 @@
     import { selectedExerciseId, previousRoute } from '../services/storelinks.js';
     import { page } from "$app/stores";
     import { goto } from "$app/navigation";
-    export let exercise = {};
+
+    export let exercise = {}; 
+    export let onExercisesCreated;
+
+    let exerciseDetails = null; 
+
+    async function getExerciseById(exerciseId) {
+        try {
+            const response = await fetch(`http://localhost:5000/api/exercise/${exerciseId}`);
+
+            if (!response.ok) {
+                throw new Error(`Erro: ${response.statusText}`);
+            }
+
+            exerciseDetails = await response.json();
+            console.log('Detalhes do exercício:', exerciseDetails);
+        } catch (error) {
+            console.error('Erro ao buscar exercício:', error);
+        }
+    }
+
+    $: {
+        if (exercise && exercise.exerciseId) {
+            getExerciseById(exercise.exerciseId);
+        }
+    }
 
     $: sessionStorage.setItem("previousRoute", $page.url.pathname);
 
@@ -11,13 +36,65 @@
         goto(newRoute);
     }
 
+    async function deleteExerciseDay(exerciseDayId) {
+        console.log('ID do exercício a ser excluído:', exerciseDayId);
+
+        try {
+            const response = await fetch(`http://localhost:5000/api/exerciseday/${exercise.id}`, {
+                method: 'DELETE',                          
+            });
+
+            if (response.ok) {
+                console.log('Exercício excluído com sucesso!');
+                onExercisesCreated();
+            } else {
+                console.log('Erro ao excluir exercício.');
+            }
+        } catch (error) {
+            console.error('Erro ao excluir exercício:', error);
+        }
+    }
+
 </script>
 
 <div class="w-full flex flex-col gap-4">
-    <a on:click={() => navigateTo("/exercise")}
+    <a on:click={() => { 
+            navigateTo("/exercise");
+            selectedExerciseId.set(exercise.id); 
+        }}
         class="w-full h-44 flex flex-col items-start justify-center p-4 rounded-xl bg-cover bg-top"  
-        style="background-image: url('http://localhost:5000/api/Files/{exercise.imagePath}')"
-        on:click={() => selectedExerciseId.set(exercise.id)}>
-        <h2 class="text-5xl">{exercise.name}</h2>
+        style="background-image: url('http://localhost:5000/api/Files/{exerciseDetails?.imagePath}')">
+        
+        {#if exerciseDetails}
+            <h2 class="text-5xl">{exerciseDetails.name}</h2>
+        {:else}
+            <h2 class="text-5xl">Carregando...</h2>
+        {/if}
     </a>
+    <button id="lixo" type="button" on:click={() => deleteExerciseDay(exerciseDetails.id)}>
+        <img src="/src/images/lixeira.svg" alt="Excluir">
+    </button>
 </div>
+
+<style>
+    #lixo {
+        position: relative;
+        top: -4rem;
+        right: -15rem;
+        background-color: #171717;
+        border-radius: 50%;
+        align-items: center;
+        justify-content: center;
+        width: 35px;
+        height: 35px;
+        border: none;
+        cursor: pointer;
+    }
+
+    #lixo img {
+        position: relative;
+        left: 5px;
+        width: 24px;
+        height: 24px;
+    }
+</style>
--- src/lib/ShowExercises.svelte
+++ src/lib/ShowExercises.svelte
@@ -1,66 +1,107 @@
 <script>
-    import { createEventDispatcher } from 'svelte';
+    export let userId;
+    export let exerciseUser = []; 
+    export let onExercisesCreated = null; 
     
-    const dispatch = createEventDispatcher();
-    
-    export let exerciseId;
-    export let userId; 
+    let exercises = [];
+    let selectedExercises = [];
+    let error = null;
 
-   
-    
-    </script>
-    
-    <form on:submit|preventDefault={handleSubmit} class="mt-4 flex flex-col gap-4">
-        <div class="flex flex-col gap-2">
-            <label>Dias de execução:</label>
-            {#each days as day}
+    async function getExercises() {
+        try {
+            const response = await fetch(`http://localhost:5000/api/exercise`);
+            if (!response.ok) {
+                throw new Error(`Erro ao buscar exercícios: ${response.statusText}`);
+            }
+            
+            const data = await response.json();
+            let allExercises = data["$values"].map(exercise => ({
+                ...exercise,
+                checked: false
+            }));
+
+            const userExerciseIds = exerciseUser.map(exerciseDay => exerciseDay.exerciseId);
+
+            exercises = allExercises.filter(exercise => !userExerciseIds.includes(exercise.id));
+
+        } catch (err) {
+            error = err.message;
+            console.error('Erro ao buscar exercícios:', error);
+        }
+    }
+
+    async function createExerciseDays() {
+        selectedExercises = exercises.filter(exercise => exercise.checked);
+        if (selectedExercises.length === 0) {
+            console.log('Por favor, selecione ao menos um exercício.');
+            return;
+        }
+
+        try {
+            const exerciseDayRequests = selectedExercises.map(exercise => {
+                const payload = {
+                    ExerciseId: exercise.id,
+                    DayOfWeek: null,
+                    SessionId: null,
+                    UserId: userId,
+                };
+
+                return fetch(`http://localhost:5000/api/exerciseday`, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(payload),
+                });
+            });
+
+            await Promise.all(exerciseDayRequests);
+
+            if (onExercisesCreated) {
+                onExercisesCreated(); 
+            }
+
+            console.log('Exercícios adicionados com sucesso');
+        } catch (error) {
+            console.error('Erro ao adicionar exercícios:', error);
+        }
+    }
+
+    $: getExercises();
+</script>
+
+
+{#if error}
+    <p class="text-red-500">Erro: {error}</p>
+{/if}
+
+<form on:submit|preventDefault={createExerciseDays} class="mt-4 flex flex-col gap-4">
+    <div class="flex flex-col gap-2">
+        <label>Adicionar exercicios:</label>
+        {#each exercises as exercise (exercise.id)}
             <label>
-                <input type="checkbox" bind:checked={day.checked} />
-                {day.name}
+                <input type="checkbox" bind:checked={exercise.checked} />
+                {exercise.name}
             </label>
-            {/each}
-        </div>
-    
-        <div class="flex flex-col gap-2">
-            <label>Tempo de sessões (minutos):</label>
-            <input type="number" bind:value={sessionTime} min="1" required />
-        </div>
-    
-        <div class="flex flex-col gap-2">
-            <label>Quantidade de Sessões:</label>
-            <input type="number" bind:value={session} min="1" required />
-        </div>
-    
-        <div class="flex flex-col gap-2">
-            <label>Quantidade de repetições:</label>
-            <input type="number" bind:value={repetitions} min="1" required />
-        </div>
-    
-        <div class="flex flex-col gap-2">
-            <label>Tempo de intervalos (minutos):</label>
-            <input type="number" bind:value={intervalTime} min="1" required />
-        </div>
-    
-        <button type="submit" class="px-4 py-2 bg-[#facc15] text-black rounded">Salvar</button>
-    </form>
-    
-    <style>
-        input[type="number"], input[type="checkbox"] {
-            background-color: transparent;
-            color: white;
-            border: 1px solid white;
-            padding: 0.5rem;
-            border-radius: 0.25rem;
-        }
-    
-        input[type="number"]::placeholder {
-            color: white;
-        }
-    
-        label {
-            display: flex;
-            align-items: center;
-            gap: 0.5rem;
-        }
-    </style>
-    
+        {/each}
+    </div>
+
+    <button type="submit" class="px-4 py-2 bg-[#facc15] text-black rounded">Salvar</button>
+</form>
+
+<style>
+    input[type="checkbox"] {
+        background-color: transparent;
+        color: white;
+        border: 1px solid white;
+        padding: 0.5rem;
+        border-radius: 0.25rem;
+    }
+
+    label {
+        display: flex;
+        align-items: center;
+        gap: 0.5rem;
+    }
+</style>
+
